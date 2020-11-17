c94b4a8ed2940fbea1d1162adc1078f7ccd92f0f
yaskevich <alyaxey.yaskevich@gmail.com>
2020-11-17 23:14:14 +0300

    update linting check for CLI

--- check.sh
+++ check.sh
@@ -1,8 +1,4 @@
-eslint scripts/download-sheets.js 
-jshint scripts/download-sheets.js 
-eslint scripts/import-sheets.js
-jshint scripts/import-sheets.js
-eslint scripts/process-descriptions.js
-jshint scripts/process-descriptions.js
+eslint cli.js 
+jshint cli.js 
 eslint index.js 
 jshint index.js 
